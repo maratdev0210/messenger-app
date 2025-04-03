d004d899c969e0afb72e7bcd4f00b3325fc94666
maratdev0210 <maratforwork1@gmail.com>
2025-04-03 13:31:50 +0300

    logout api route

--- src/widgets/Chats/ProfileInfo/TooltipProfile.tsx
+++ src/widgets/Chats/ProfileInfo/TooltipProfile.tsx
@@ -6,6 +6,9 @@ import {
 } from "@/components/ui/tooltip";
 import { useNavigate } from "react-router-dom";
 import { Pencil, LogOut } from "lucide-react";
+import { LOGOUT_ROUTE } from "@/utils/constants";
+import { apiClient } from "@/lib/apiClient";
+import { useAppStore } from "@/store/store";
 
 interface ITooltipProfile {
   text: string;
@@ -14,6 +17,23 @@ interface ITooltipProfile {
 
 export default function TooltipProfile({ text, iconCode }: ITooltipProfile) {
   const navigate = useNavigate();
+  const { setUserInfo } = useAppStore();
+  const logout = async () => {
+    try {
+      const response = await apiClient.post(
+        LOGOUT_ROUTE,
+        {},
+        { withCredentials: true }
+      );
+
+      if (response.status === 200) {
+        setUserInfo(undefined);
+        navigate("/auth");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <TooltipProvider>
@@ -26,8 +46,8 @@ export default function TooltipProfile({ text, iconCode }: ITooltipProfile) {
               />
             ) : (
               <LogOut
-                onClick={() => navigate("/profile")}
-                className="cursor-pointer text-blue-500"
+                onClick={logout}
+                className="cursor-pointer text-red-500"
               />
             )}
           </TooltipTrigger>
